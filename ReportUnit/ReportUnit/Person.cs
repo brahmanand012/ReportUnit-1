cf7c0f677e53ee5549c937222d444aff8fd527bf
Sean Rand <srand@nephilacapital.com>
2016-03-20 16:39:57 -0300

    adding mstests

---
+++ ReportUnit/ReportUnit/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportUnit
+{
+    public class Person
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private string HairColour { get; set; }
+        private string EyeColour { get; set; }
+
+        public Person(string name, int age, string eyeColour, string hairColour)
+        {
+            Name = name;
+            Age = age;
+            HairColour = hairColour;
+            EyeColour = eyeColour;
+        }
+    }
+}
---
+++ ReportUnit/UnitTests/PersonTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void TestMethod6()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.AreEqual("Test", "Fail");
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.AreEqual("Test", "Fail");
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Assert.AreEqual("Test", "Fail");
+
+        }
+
+
+        [ClassCleanup()]
+        public static void ReportUnit()
+        {
+
+            System.Console.WriteLine("Test");
+        }
+    }
+}
